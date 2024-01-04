8167560d235ee851c799448237a674455b346f13
Rob Young <rhyoung214@gmail.com>
2024-01-03 21:13:52 -0800

    Completed chapter 2
    Learn how to run test with different formats,
    - run test with detailed output
    - run test that output to html file
    - run previously failed test
    - run a specific test

---
+++ 02-running-specs/09/spec/coffe_spec.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+class Coffee
+  def ingredients
+    @ingredients ||= []
+  end
+
+  def add(ingredient)
+    ingredients << ingredient
+  end
+
+  def price
+    1.00 + ingredients.size * 0.25
+  end
+end
+
+RSpec.configure do |config|
+  config.filter_run_when_matching(focus: true)
+  config.example_status_persistence_file_path = 'spec/examples.txt'
+end
+
+RSpec.describe 'A cup of coffee' do
+  let(:coffee) { Coffee.new }
+
+  it "costs $1" do
+    expect(coffee.price).to eql(1.00)
+  end
+
+  context "with milk", focus: true do
+    before { coffee.add :milk }
+
+    it "cost $1.25" do
+      expect(coffee.price).to eq(1.25)
+    end
+
+    it "is light in color"
+    it "is cooler than 200 degree Fahrenheit"
+  end
+end
